2e6924eda45c3744a0c98bb34434195eb601ca3d
linhtuan152 <36038789+linhtuan152@users.noreply.github.com>
2020-01-14 13:17:27 +0700

    add sso

---
+++ src/appRedux/actions/GetUser.js
@@ -0,0 +1,45 @@
+import {
+  GET_USER_DATA_START,
+  GET_USER_DATA_SUCCESS,
+  GET_USER_DATA_ERROR
+} from "constants/ActionTypes";
+import axios from "util/TcApp";
+export const getUserData = () => {
+  console.log("in");
+  return dispatch => {
+    dispatch({ type: GET_USER_DATA_START });
+    // console.log(document.cookie.indexOf("request_token"));
+    axios
+      .get(
+        "users/" +
+          document.cookie
+            .split(";")
+            [document.cookie.indexOf("user_id")].split("=")[1],
+        {
+          headers: {
+            Authorization:
+              "Bearer " +
+              document.cookie
+                .split(";")
+                [1 - document.cookie.indexOf("user_id")].split("=")[1]
+          }
+        }
+      )
+      .then(res => {
+        localStorage.setItem("user_info", JSON.stringify(res.data));
+      })
+      .then(res => {
+        dispatch({
+          type: GET_USER_DATA_SUCCESS,
+          payload: res.data
+        });
+      })
+      .catch(error => dispatch({ type: GET_USER_DATA_ERROR, payload: error }));
+  };
+};
+
+export const getUserDataSuccess = () => {
+  return dispatch => {
+    dispatch({ type: GET_USER_DATA_SUCCESS });
+  };
+};
--- src/appRedux/reducers/index.js
+++ src/appRedux/reducers/index.js
@@ -7,12 +7,14 @@ import Account from "./Account";
 import User from "./User";
 import Step from "./Step";
 import CompanyProfile from "./CompanyProfile";
+import GetUser from "./GetUser";
 
 const reducers = combineReducers({
   routing: routerReducer,
   settings: Settings,
   auth: Auth,
   commonData: Common,
+  getUser: GetUser,
   Account,
   User,
   Step,
